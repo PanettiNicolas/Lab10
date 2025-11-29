9754ad5b781797a7d97903dd6af0761fa91c8ed4
Nicolas Panetti <panettinico@gmail.com>
2025-11-29 15:21:43 +0100

    Rifinimenti finali

--- model/model.py
+++ model/model.py
@@ -5,6 +5,7 @@ class Model:
     def __init__(self):
         #self._nodes = None
         #self._edges = None
+        self.G = nx.Graph()
         self._lista_hub = []         #Nodes
         self._lista_tratte = []      #Edges
         self._dizionario_hub = {}
@@ -15,17 +16,17 @@ class Model:
         guadagno medio per spedizione >= threshold (euro)
         """
         # TODO
-        self.G = nx.Graph()
+        self.G.clear()                    #Pulisco il grafo in modo da non sovrascriverlo ogni volta
         self._lista_hub = DAO.get_hub()          #Lista di oggetti Hub
         self._lista_tratte = DAO.get_tratta()    #Lista di oggetti Tratta
 
-        for hub in self._lista_hub:
-            self.G.add_node(hub)
-            self._dizionario_hub[hub.id] = hub
+        for hub in self._lista_hub:       #Ciclo sui nodi
+            self.G.add_node(hub)                     #Aggiungo ogni hub alla lista dei nodi
+            self._dizionario_hub[hub.id] = hub       #Creo il dizionario di hub per poter aggiungere più facilmente le tratte
 
 
-        for tratta in self._lista_tratte:
-            if tratta.guadagno_medio >= threshold:
+        for tratta in self._lista_tratte:            #Ciclo sulle tratte
+            if tratta.guadagno_medio >= threshold:   #Condizione sul guadagno minimo
                 self.G.add_edge(self._dizionario_hub[tratta.id_hub_A], self._dizionario_hub[tratta.id_hub_B], weight=tratta.guadagno_medio)
 
         return self.G
